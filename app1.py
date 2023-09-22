af9cbe9fe92210da2910b6c07ac2c549f3114c09
mailchecker <zinux@naver.com>
2023-09-22 17:43:16 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -16,11 +16,20 @@ for i in range(num_players):
     strokes = col2.number_input("Strokes", value=0, key=f"strokes_{i}")
     handicap = col3.number_input("Handicap", value=0.0, key=f"handicap_{i}")
     net = strokes - handicap
-    col4.text(f"Net: {net}")
+    
+    # Net을 읽기 전용 number_input으로 표시합니다. 
+    # max_value와 min_value를 동일한 값으로 설정하여 값 변경을 방지합니다.
+    col4.number_input("Net", value=net, min_value=net, max_value=net, key=f"net_{i}")
+    
     players.append({'name': name, 'net': net, 'strokes': strokes, 'handicap': handicap})
 
-if st.button('Sort'):
+# Sort 버튼과 Reset 버튼을 같은 줄에 위치시킵니다.
+col1, col2 = st.columns(2)
+if col1.button('Sort'):
     players = sorted(players, key=itemgetter('net'))
+
+if col2.button('Reset'):
+    st.experimental_rerun()
 
 # 하위 순위의 플레이어들을 출력합니다.
 bottom_players = players[-num_bottom:]
@@ -30,6 +39,3 @@ st.write('성적하위:')
 
 for player in bottom_players:
     st.write(f"{player['name']} - Strokes: {player['strokes']}, Handicap: {player['handicap']}, Net: {player['net']}")
-
-if st.button('Reset'):
-    st.experimental_rerun()
