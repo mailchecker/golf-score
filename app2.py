fc0b9c7ec02ed0e876073fc5d0714a3ae4575df8
mailchecker <zinux@naver.com>
2023-12-21 17:15:59 +0900

    Update app2.py

--- app2.py
+++ app2.py
@@ -86,15 +86,15 @@ if selected_date:
     df = pd.DataFrame(selected_data)
 
 
-    # 열 삭제 기능
-    if len(df.columns) > 0:
-        delete_col_name = st.selectbox("삭제할 열 선택", options=df.columns)
-        if st.button("열 삭제"):
-            if delete_col_name in df.columns:
-                df = df.drop(columns=[delete_col_name])  # 열 삭제
-                data[selected_date] = df.to_dict('records')  # 변경된 데이터를 JSON으로 다시 저장
-                write_json("golfers_data.json", data)
-                st.experimental_rerun()
+    # 골퍼 삭제 기능
+    golfer_names = df['name'].tolist()  # 골퍼 이름 목록
+    delete_golfer_name = st.selectbox("삭제할 골퍼 선택", options=golfer_names)
+    if st.button("골퍼 삭제"):
+        # 선택된 골퍼 이름에 해당하는 데이터를 삭제
+        df = df[df['name'] != delete_golfer_name]
+        data[selected_date] = df.to_dict('records')  # 변경된 데이터를 JSON으로 다시 저장
+        write_json("golfers_data.json", data)
+        st.experimental_rerun()
 
     
     
