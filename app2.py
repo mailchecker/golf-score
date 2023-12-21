ca346d78bbd1cf2a86711547eb02372eec6bf82f
mailchecker <zinux@naver.com>
2023-12-21 13:12:19 +0900

    Update app2.py

--- app2.py
+++ app2.py
@@ -45,8 +45,8 @@ for i in range(number_of_golfers):
     with col3:
         handicap = st.number_input(f'핸디캡', min_value=0, key=f'handicap_{i}', on_change=update_result, args=(i,))
     with col4:
-        # '최종결과' 읽기 전용 입력칸
-        result = st.session_state.get(f'result_{i}', 0)
+        # '최종결과'를 문자열로 변환하여 전달
+        result = str(st.session_state.get(f'result_{i}', 0))
         st.text_input('최종결과', value=result, key=f'result_{i}', disabled=True)
 
     golfers_data.append({'name': name, 'stroke': stroke, 'handicap': handicap, 'result': result})
