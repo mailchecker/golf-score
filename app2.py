17caf911073a5c56a6a66b21ea02da142d1e254b
mailchecker <zinux@naver.com>
2024-01-12 17:59:18 +0900

    Update app2.py

--- app2.py
+++ app2.py
@@ -6,7 +6,7 @@ import os
 
 
 # Everything is accessible via the st.secrets dict:
-st.write("DB username:", st.secrets["DB_USERNAME"])
+st.write("DB username:", st.secrets["db_username"])
 st.write("DB password:", st.secrets["db_password"])
 st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
 
