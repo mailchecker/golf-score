b9b27be8e7f6219e49ec08803abf3dfad8f3c3a6
mailchecker <zinux@naver.com>
2024-01-12 17:56:27 +0900

    Update app2.py

--- app2.py
+++ app2.py
@@ -5,11 +5,16 @@ from datetime import datetime
 import os
 
 
-st.write("DB username:", st.secrets["DB_USERNAME"])
-st.write("DB_TOKEN:", st.secrets["DB_TOKEN"])
-st.write("some_section:", st.secrets["some_section"]["some_key"])
-st.write("SPB_URL:", st.secrets["SPB_URL"])
+# Everything is accessible via the st.secrets dict:
+st.write("DB username:", st.secrets["db_username"])
+st.write("DB password:", st.secrets["db_password"])
+st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
 
+# And the root-level secrets are also accessible as environment variables:
+st.write(
+    "Has environment variables been set:",
+    os.environ["db_username"] == st.secrets["db_username"],
+)
 
 
 
