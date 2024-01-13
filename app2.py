7a0f6b311515341104fdba87e368733b70231269
mailchecker <zinux@naver.com>
2024-01-13 13:17:53 +0900

    Update app2.py

--- app2.py
+++ app2.py
@@ -53,13 +53,18 @@ def read_data_test(selected_date=None):
 def write_data(golfer_data):
     try:
         st.write(golfer_data)
-        response = conn.table("golf_scores").insert(golfer_data, count="None").execute()
+        # 날짜 객체를 ISO 포맷 문자열로 변환
+        if 'date' in golfer_data and isinstance(golfer_data['date'], datetime.date):
+            golfer_data['date'] = golfer_data['date'].isoformat()
+
+        response = st_supabase.table("golf_scores").insert([golfer_data], count="None").execute()
         if response.error:
             st.error(f"데이터 삽입 오류: {response.error}")
         else:
             st.success("골퍼 정보가 저장되었습니다.")
     except Exception as e:
         st.error(f"데이터 쓰기 오류: {e}")
+
 
 
 # 데이터베이스에서 데이터 삭제
