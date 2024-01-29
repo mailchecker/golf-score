c7c9194522b6a85ccef0f8f1dc0ccab5bb201684
mailchecker <zinux@naver.com>
2024-01-29 11:16:28 +0900

    Update app2.py

--- app2.py
+++ app2.py
@@ -35,7 +35,7 @@ def read_group_date():
     year = 2024
     query = (
         conn.table("golf_scores")
-        .select("date, count(name)", count="exact")
+        .select("date")
         .order("date")
     )
     
